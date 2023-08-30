28817605fcd04714f72fe30ae66c8c58e6eff50e
Ying Xin <chenyingxin92@gmail.com>
2023-08-30 11:11:27 -0400

    recommit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,9 @@ const App = () => {
     const movieFavorites = JSON.parse(
       localStorage.getItem("react-movie-app-favorites")
     );
-    setFavorites(movieFavorites);
+    if (movieFavorites) {
+      setFavorites(movieFavorites);
+    }
   }, []);
 
   const saveToLocalStorage = (items) => {
